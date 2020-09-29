ab67524126a4cc80246221cb7fe2860fcd21b90f
TheSlaylord <abevnyaev@gmail.com>
2020-09-29 21:48:57 +0300

    it's probably done

---
+++ scripts/main.js
@@ -0,0 +1,38 @@
+UnitTypes.dagger.health = 280;
+UnitTypes.mace.health = 1000;
+UnitTypes.fortress.health = 1580;
+UnitTypes.scepter.health = 18000;
+UnitTypes.reign.health = 48000;
+
+UnitTypes.nova.health = 220;
+UnitTypes.pulsar.health = 640;
+UnitTypes.quasar.health = 1100;
+UnitTypes.vela.health = 11000;
+UnitTypes.corvus.health = 38000;
+
+UnitTypes.crawler.health = 360;
+UnitTypes.atrax.health = 1200;
+UnitTypes.spiroct.health = 1520;
+UnitTypes.arkyid.health = 16000;
+UnitTypes.toxopid.health = 44000;
+
+UnitTypes.flare.health = 150;
+UnitTypes.horizon.health = 700;
+UnitTypes.zenith.health = 1400;
+UnitTypes.antumbra.health = 14000;
+UnitTypes.eclipse.health = 40000;
+
+UnitTypes.mono.health = 200;
+UnitTypes.poly.health = 800;
+UnitTypes.mega.health = 1000;
+UnitTypes.quad.health = 12000;
+UnitTypes.oct.health = 46000;
+
+UnitTypes.risso.health = 560;
+UnitTypes.minke.health = 1200;
+UnitTypes.bryde.health = 1800;
+UnitTypes.sei.health = 20000;
+UnitTypes.omura.health = 44000;
+
+//just in some useless case lol
+UnitTypes.block.health = 2;
